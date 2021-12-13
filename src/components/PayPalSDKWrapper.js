04b538a208120dcdce2bbb7b66586a30c91e9d7d
Awesome CodeClimber <95038746+AwesomeCodeClimber@users.noreply.github.com>
2021-12-13 07:50:06 +0300

    Initial commit

---
+++ src/components/PayPalSDKWrapper.js
@@ -0,0 +1,90 @@
+// @flow
+/* eslint-disable react/require-default-props */
+import { useState, useEffect } from 'react';
+import queryString from 'query-string';
+import humps from 'humps';
+
+// See SDK parameters list:
+// https://developer.paypal.com/docs/checkout/reference/customize-sdk/
+
+type Props = {
+  clientId: string,
+  merchantId?: string,
+  intent?: 'capture' | 'authorize',
+  commit?: boolean,
+  vault?: boolean,
+  components?: string,
+  currency?: string,
+  disableFunding?: Array<'credit' | 'card' | 'sepa'>,
+  disableCard?: Array<'visa' | 'mastercard' | 'amex' | 'discover' | 'jcb' | 'elo' | 'hiper'>,
+  integrationDate?: string,
+  locale?: string,
+  buyerCountry?: string,
+  debug: boolean,
+  loading?: React$Node,
+  children: React$Element<any>,
+};
+
+function PayPalSDKWrapper(props: Props) {
+  const {
+    loading,
+    children,
+    ...config
+  } = props;
+
+  const [isScriptLoaded, setIsScriptLoaded] = useState(false);
+
+  function handleScriptLoaded() {
+    setIsScriptLoaded(true);
+  }
+
+  function handleScriptError() {
+    // Error loading PayPal SDK script.
+  }
+
+  function getSDKParams(): Object {
+    const {
+      clientId,
+      disableFunding,
+      disableCard,
+      ...rest
+    } = config;
+
+    const params = {
+      clientId: clientId || process.env.REACT_APP_PAYPAL_CLIENT_ID,
+      disableFunding: disableFunding && disableFunding.join(','),
+      disableCard: disableCard && disableCard.join(','),
+      ...rest,
+    };
+
+    return humps.decamelizeKeys(params, { separator: '-' });
+  }
+
+  useEffect(() => {
+    const params = getSDKParams();
+    const src = `https://www.paypal.com/sdk/js?${queryString.stringify(params)}`;
+
+    const scriptElement = document.createElement('script');
+    scriptElement.src = src;
+
+    scriptElement.addEventListener('load', handleScriptLoaded);
+    scriptElement.addEventListener('error', handleScriptError);
+
+    if (document.body) {
+      document.body.appendChild(scriptElement);
+    }
+
+    return () => {
+      scriptElement.removeEventListener('load', handleScriptLoaded);
+      scriptElement.removeEventListener('error', handleScriptError);
+    };
+  }, []);
+
+  if (!isScriptLoaded) {
+    return loading || null;
+  }
+
+  return children;
+}
+
+export default PayPalSDKWrapper;
---
+++ tests/PayPalSDKWrapper.test.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import queryString from 'query-string';
+import {
+  render,
+  fireEvent,
+  cleanup,
+} from 'react-testing-library';
+import { PayPalSDKWrapper } from '../src';
+
+const sdkUrl = 'https://www.paypal.com/sdk/js';
+
+describe('<PayPalSDKWrapper />', () => {
+  let scriptElement;
+
+  afterEach(cleanup);
+
+  beforeEach(() => {
+    render((
+      <PayPalSDKWrapper
+        clientId="CLIENT_ID"
+        disableFunding={['card', 'sepa']}
+        disableCard={['visa', 'mastercard']}
+        loading="Loading..."
+      >
+        <span data-testid="child">Some Child</span>
+      </PayPalSDKWrapper>
+    ));
+
+    scriptElement = document.querySelector(
+      `script[src^='${sdkUrl}']`,
+    );
+
+    fireEvent.load(scriptElement);
+  });
+
+  it('loads the paypal javascript sdk script', () => {
+    expect(scriptElement).toBeTruthy();
+  });
+
+  it('loads the paypal javascript sdk script with the correct parameters', () => {
+    const query = scriptElement.src.split('?')[1];
+    const usedParams = queryString.parse(query);
+
+    expect(usedParams).toEqual({
+      'client-id': 'CLIENT_ID',
+      'disable-funding': 'card,sepa',
+      'disable-card': 'visa,mastercard',
+    });
+  });
+});
